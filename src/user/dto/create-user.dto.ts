b2f0ab9527749a50d47d59add61fc0b8a7893148
Emerson Dantas <emerson.dantaspereira@hotmail.com>
2022-05-03 20:49:09 -0300

    add Entity and CreateDto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,45 @@
-export class CreateUserDto {}
+import { User } from '../entities/user.entity';
+import { IsBoolean, IsDate, IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class CreateUserDto extends User {
+    @IsString()
+    @MinLength(2)
+    @MaxLength(100)
+    name: string;
+
+    @IsString()
+    birthdate: string;
+
+    @IsString()
+    document: string;
+
+    @IsBoolean()
+    acceptedTerms: boolean;
+
+    @IsString()
+    zipcode: string;
+
+    @IsString()
+    @IsOptional()
+    street?: string;
+
+    @IsString()
+    @IsOptional()
+    neighborhood?: string;
+
+    @IsString()
+    @IsOptional()
+    city?: string;
+
+    @IsString()
+    @IsOptional()
+    state?: string;
+
+    @IsDate()
+    @IsOptional()
+    createdAt?: Date | string;
+
+    @IsDate()
+    @IsOptional()
+    updatedAt?: Date | string;
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,15 @@
-export class User {}
+import { Prisma } from '@prisma/client'
+
+export class User implements Prisma.UserUncheckedCreateInput{
+    name: string;
+    birthdate: string;
+    document: string;
+    acceptedTerms: boolean;
+    zipcode: string;
+    street?: string;
+    neighborhood?: string;
+    city?: string;
+    state?: string;
+    createdAt?: Date | string;
+    updatedAt?: Date | string;
+}
